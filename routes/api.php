873dde0324a31c550f9bd203a1e02c5e701615a7
Guilherme Seganzerla <g.seganzerla@gmail.com>
2022-02-22 17:39:08 -0300

    Revert "feat: auth"
    This reverts commit 0c972be8dbcd5dcdc01ea765303177d57a485dbd.

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,4 @@ use App\Http\Controllers\{
 };
 use Illuminate\Support\Facades\Route;
 
-Route::controller(AuthController::class, function () {
-    Route::post('/auth/register', 'register')->name('auth.register');
-    Route::post('/auth/login', 'login')->name('auth.login');
-});
-
-
-Route::group(['middleware' => 'auth:sanctum'], function () {
-    Route::apiResource('accounts', AccountController::class);
-});
+Route::apiResource('accounts', AccountController::class);
