8422b5a782de20f181a256bf2b3390c4305b62b6
Guilherme Seganzerla <g.seganzerla@gmail.com>
2022-02-23 16:37:11 -0300

    feat: route fixing

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,17 @@
 
 use App\Http\Controllers\{
     AccountController,
+    AuthController,
     UserController
 };
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('accounts', AccountController::class);
-Route::apiResource('users', UserController::class);
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/auth/register', 'register')->name('auth.register');
+    Route::post('/auth/login', 'login')->name('auth.login');
+});
+
+Route::group(['prefix' => 'home', 'middleware' => 'auth:sanctum'], function () {
+    Route::apiResource('accounts', AccountController::class);
+    Route::apiResource('users', UserController::class);
+});
